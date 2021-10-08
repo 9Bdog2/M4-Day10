74f9081df194dabd3df948c874d103bee7bf5ce6
Bogdan Birau <birau.bogdan92@gmail.com>
2021-10-08 13:02:55 +0100

    Converted first page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
-import './App.css';
-import "react-bootstrap"
-import LeftSidebar from './components/LeftSidebar'
-import BottomNavbar from './components/BottomNavbar'
-import TopNavbar from './components/TopNavbar'
-import Body from './components/Body'
-
-
-
-
+import "./App.css";
+import "react-bootstrap";
+import LeftSidebar from "./components/LeftSidebar";
+import BottomNavbar from "./components/BottomNavbar";
+import TopNavbar from "./components/TopNavbar";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
       <div className="container-fluid">
-      <TopNavbar />
-      <LeftSidebar />
-      <Body />
-      <BottomNavbar />
-      
+        <TopNavbar />
+        <LeftSidebar />
+        <Body />
+        <BottomNavbar />
       </div>
     </div>
   );
